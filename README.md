c171e11ef2a0072172f7b3dbeaac560dd24e50ae
VadimToptunov <umnijtony@mail.ru>
2021-07-06 15:43:35 +0300

    Create boilerplate for Page Object Model tests

---
+++ README.md
@@ -0,0 +1,4 @@
+# Задание по курсу "Автоматизация тестирования с помощью Selenium и Python"
+## Применение паттерна Page Object Model
+Запуск теста производится следующим образом:
+`pytest -v --language=en test_main_page.py`
---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help="Set a locale.")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    locale = request.config.getoption("language")
+    options = Options()
+    options.add_argument("--start-maximized")
+    options.add_experimental_option('prefs', {'intl.accept_languages': locale})
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(chrome_options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
