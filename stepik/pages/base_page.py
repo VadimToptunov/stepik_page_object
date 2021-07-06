e984d155e17e15c056df004f1e87b86ac9a662b3
VadimToptunov <umnijtony@mail.ru>
2021-07-06 19:40:04 +0300

    Implement method to check if element present on page

--- stepik/pages/base_page.py
+++ stepik/pages/base_page.py
@@ -1,14 +1,26 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage(object):
 	"""
 	Base page for Page Object Model Selenium tests witth Python.
 	"""
-	def __init__(self, browser, url):
+	def __init__(self, browser, url, timeout=10):
 		self.browser = browser
 		self.url = url
+		self.browser.implicitly_wait(timeout)
 
 
 	def open(self):
 		self.browser.get(self.url)
+
+
+	def is_element_present(self, how, what):
+		try:
+			self.browser.find_element(how, what)
+		except NoSuchElementException:
+			return False
+		return True
 
 
 	def quit(self):
