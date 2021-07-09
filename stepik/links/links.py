f37352c3b10e7dadc2b0e985222107e34333053f
VadimToptunov <umnijtony@mail.ru>
2021-07-09 19:23:19 +0300

    Add parametrization to product test, modify links

--- stepik/links/links.py
+++ stepik/links/links.py
@@ -1,4 +1,5 @@
 class ProjectLinks():
 	MAIN_PAGE_LINK = "http://selenium1py.pythonanywhere.com/"
+	PROMO_LINK_TEMPLATE = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo="
 	# NEW_YEAR_PROMO_LINK = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 	NEW_YEAR_PROMO_LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
--- stepik/test_product_page.py
+++ stepik/test_product_page.py
@@ -1,11 +1,14 @@
 from links.links import ProjectLinks
 from pages.product_page import ProductPage
 from pages.locators import ProductPageLocators
-import time
+import pytest
 
-
-def test_guest_can_add_product_to_basket(browser):
-	product_page = ProductPage(browser, ProjectLinks.NEW_YEAR_PROMO_LINK)
+@pytest.mark.parametrize('link', ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6",
+                                  pytest.param("offer7", marks=pytest.mark.xfail),
+                                  "offer8", "offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+	url = f"{ProjectLinks.PROMO_LINK_TEMPLATE}{link}"
+	product_page = ProductPage(browser, url)
 	product_page.open()
 	product_page.should_be_product_page()
 	data = product_page.get_product_name_and_price()
