4b5b972b8692620e5e86dd7ea233584007861e78
VadimToptunov <umnijtony@mail.ru>
2021-07-09 15:10:36 +0300

    Add passing the pages, add class for links enumeration

---
+++ stepik/links/links.py
@@ -0,0 +1,3 @@
+class ProjectLinks():
+	MAIN_PAGE_LINK = "http://selenium1py.pythonanywhere.com/"
+		
--- stepik/test_main_page.py
+++ stepik/test_main_page.py
@@ -1,19 +1,18 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from links.links import ProjectLinks
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, ProjectLinks.MAIN_PAGE_LINK)
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
 
 
 def test_guest_can_see_correct_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, ProjectLinks.MAIN_PAGE_LINK)
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, ProjectLinks.MAIN_PAGE_LINK)
     login_page.should_be_login_page()
